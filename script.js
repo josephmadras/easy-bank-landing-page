3cdacf42cd1708d57b257822b829ddb0a2faf77c
Joseph Madras <josephkabemba77@gmail.com>
2021-11-29 18:38:15 +0100

    feat: add some js script to toggle the menu on small devices

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector("#hamburger-container");
+const nav = document.querySelector("#nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("mobile-nav");
+  // alert("Joseph Madras");
+});
