2f94d381e99c2ea845599257026c84d19a7860a3
Joseph Madras <josephkabemba77@gmail.com>
2021-11-29 21:36:40 +0100

    refactor: remove comments

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// const hamburger = document.querySelector("#hamburger-container");
 const nav = document.querySelector("#nav");
-
-// hamburger.addEventListener("click", () => {
-//   nav.classList.toggle("mobile-nav");
-//   // alert("Joseph Madras");
-// });
-
 const menuBtn = document.querySelector(".hamburger-container");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
-  // alert("Joseph Madras de Luluabourg");
   nav.classList.toggle("mobile-nav");
   if (!menuOpen) {
     menuBtn.classList.add("open");
