6911a9a30b55bd883f9dc37c0cffe81575a679bd
Joseph Madras <josephkabemba77@gmail.com>
2021-11-29 21:27:42 +0100

    feat: animate and transform the the hamburger to close icon  on click

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const hamburger = document.querySelector("#hamburger-container");
+// const hamburger = document.querySelector("#hamburger-container");
 const nav = document.querySelector("#nav");
 
-hamburger.addEventListener("click", () => {
+// hamburger.addEventListener("click", () => {
+//   nav.classList.toggle("mobile-nav");
+//   // alert("Joseph Madras");
+// });
+
+const menuBtn = document.querySelector(".hamburger-container");
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+  // alert("Joseph Madras de Luluabourg");
   nav.classList.toggle("mobile-nav");
-  // alert("Joseph Madras");
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
 });
